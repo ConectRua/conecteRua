d261ee50f87c0d282d794d228573360327b62dc0
FellipeMarques06 <dosesdetox@gmail.com>
2025-09-14 16:23:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+  server:  {
+  allowedHosts: ['.replit.dev']
+}
     host: "::",
     port: 8080,
   },
