6786608d0fd4e904a5ba79d32cc4f49f161e9a73
FellipeMarques06 <dosesdetox@gmail.com>
2025-10-10 15:22:19 -0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+iimport { defineConfig, loadEnv } from "vite"; // <-- A ÚNICA MUDANÇA É AQUI
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     server: {
@@ -25,10 +25,12 @@ export default defineConfig(({ mode }) => {
         "f4188f4d-b026-473e-945a-5a0b13d4deff-00-cvhn23q3ysq7.janeway.replit.dev",
         ".replit.dev",
         ".repl.co",
-        "localhost"
+        "localhost",
       ],
     },
-    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+      Boolean
+    ),
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
